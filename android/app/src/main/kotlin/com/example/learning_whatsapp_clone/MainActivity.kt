e8d5aab9a0fb0cebf7ff7ff4899a16b945ec9ffb
Ayush Singh <ayushsingh2357@gmail.com>
2022-10-07 15:02:12 +0530

    made app till mobile layout

---
+++ android/app/src/main/kotlin/com/example/learning_whatsapp_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learning_whatsapp_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/mobile_screen_layout.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:learning_whatsapp_clone/colors.dart';
+import 'package:learning_whatsapp_clone/widgets/contacts_list.dart';
+
+class MobileScreen extends StatelessWidget {
+  const MobileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: appBarColor,
+            title: const Text(
+              "Whatsapp",
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            centerTitle: false,
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.more_vert,
+                    color: Colors.grey,
+                  )),
+            ],
+            bottom: const TabBar(
+                indicatorColor: tabColor,
+                indicatorWeight: 4,
+                labelColor: tabColor,
+                unselectedLabelColor: Colors.grey,
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+                tabs: [
+                  Tab(
+                    text: "CHATS",
+                  ),
+                  Tab(
+                    text: "STATUS",
+                  ),
+                  Tab(
+                    text: "CALLS",
+                  ),
+                ]),
+          ),
+          body: const ContactsList(),
+        ));
+  }
+}
